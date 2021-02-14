5159a47b88529e3a5f31ad52dd52f435e9440fe7
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-14 10:14:50 +0800

    Update diy-3.sh

--- build/Project_openwrt/diy-3.sh
+++ build/Project_openwrt/diy-3.sh
@@ -5,9 +5,9 @@
 
 Diy_Core() {
 	Author="281677160"
-	Default_Device="phicomm-k3"
-	Updete_firmware="phicomm-k3-squashfs.trx"
-	Extension=".trx"
+	Default_Device="x86-64"
+	Updete_firmware="openwrt-x86-64-combined-squashfs.img.gz"
+	Extension=".img.gz"
 	Source="Project"
 }
 
