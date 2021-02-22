24665b57b8cb04a75d9b72f4014a30f39c98a202
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-22 14:20:32 +0800

    Update AutoUpdate.sh

--- build/AutoUpdate/AutoUpdate.sh
+++ build/AutoUpdate/AutoUpdate.sh
@@ -77,6 +77,7 @@ CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
 Github="$(awk 'NR==2' /etc/openwrt_info)"
 DEFAULT_Device="$(awk 'NR==3' /etc/openwrt_info)"
 Firmware_Type="$(awk 'NR==4' /etc/openwrt_info)"
+Source="$(awk 'NR==5' /etc/openwrt_info)"
 TMP_Available="$(df -m | grep "/tmp" | awk '{print $4}' | awk 'NR==1')"
 Overlay_Available="$(df -h | grep ":/overlay" | awk '{print $4}' | awk 'NR==1')"
 case ${DEFAULT_Device} in
@@ -189,13 +190,13 @@ if [[ ! "$?" == 0 ]];then
 	exit
 fi
 TIME && echo "正在获取云端固件版本..."
-GET_Firmware=$(cat /tmp/Github_Tags | egrep -o "AutoBuild-${CURRENT_Device}-R[0-9].+-[0-9]+${Firmware_SFX}" | awk 'END {print}')
+GET_Firmware=$(cat /tmp/Github_Tags | egrep -o "${Source}-${CURRENT_Device}-R[0-9].+-[0-9]+${Firmware_SFX}" | awk 'END {print}')
 GET_Version=$(echo ${GET_Firmware} | egrep -o "R[0-9].+-[0-9]+")
 if [[ -z "${GET_Firmware}" ]] || [[ -z "${GET_Version}" ]];then
 	TIME && echo "云端固件版本获取失败!"
 	exit
 fi
-Firmware_Info="$(echo ${GET_Firmware} | egrep -o "AutoBuild-${CURRENT_Device}-R[0-9].+-[0-9]+")"
+Firmware_Info="$(echo ${GET_Firmware} | egrep -o "${Source}-${CURRENT_Device}-R[0-9].+-[0-9]+")"
 Firmware="${GET_Firmware}"
 Firmware_Detail="${Firmware_Info}${Detail_SFX}"
 echo -e "\n固件作者: ${Author%/*}"
