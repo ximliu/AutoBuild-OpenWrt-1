49357f13a0f9145b3dbf6770a88c1a0ccd7499f5
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-23 20:34:54 +0800

    Update upgrade.sh

--- build/AutoUpdate/upgrade.sh
+++ build/AutoUpdate/upgrade.sh
@@ -32,11 +32,11 @@ GET_TARGET_INFO() {
 	case "${TARGET_PROFILE}" in
 	x86_64)
 		GZIP="$(grep "CONFIG_TARGET_IMAGES_GZIP" ${Home}/.config)"
-		IMAGES_GZIP="# CONFIG_TARGET_IMAGES_GZIP is not set"
-		if [[ "${GZIP}" == "# CONFIG_TARGET_IMAGES_GZIP is not set" ]];then
-			Firmware_sfx="img"
-		else
+		IMAGES_GZIP="CONFIG_TARGET_IMAGES_GZIP=y"
+		if [[ "${GZIP}" == "${IMAGES_GZIP}" ]];then
 			Firmware_sfx="img.gz"
+		else
+			Firmware_sfx="img"
 		fi
 	;;
 	*)
